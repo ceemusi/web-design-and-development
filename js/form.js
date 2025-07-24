1e0d32e0032218f1f9e26fcd61c3933747aa2f6e
ceemusi <christophermusitong@gmail.com>
2025-07-24 21:35:15 +0100

    Added comments to the file

--- js/form.js
+++ js/form.js
@@ -1,4 +1,6 @@
 // This was coded by CM x24103446
+
+// Created a function called validate form, 'contactForm' is the form name from the HTML and the ID & name is 'fname' for the properties. It then uses an IF statement for if the user tries to submit nothing, an alert will pop up and tell them that they need to enter the rquired information.
 function validateForm() {
   let userInput = document.forms["contactForm"]["fname"].value;
   if (userInput == "") {
