83e323631b223f1061c45ea4f67b2c060ec69998
ceemusi <christophermusitong@gmail.com>
2025-07-26 17:54:42 +0100

    Added JS that displays greeting to user based on time of day and updates comments

--- js/date.js
+++ js/date.js
@@ -1,9 +1,12 @@
 // This was coded by CM x24103446
 
+// This creates a Date object that ulls whatever the current hour is and stores it in a const called time
 const time = new Date().getHours();
 
+// Then declared a variable called greet
 let greet;
 
+// The if statement, if the time is less than 12 hours, good morning, then it checks if its before 6pm or else if it don't match those conditions it will say good evening
 if (time < 12) {
     greet = "Good morning!";
 } else if (time < 18) {
@@ -12,4 +15,5 @@ if (time < 12) {
     greet = "Good evening!";
 }
 
+// Grabs the element 'showTime' from the HTML and displays whatever is in greet based on the time
 document.getElementById("showTime").innerHTML = greet;
