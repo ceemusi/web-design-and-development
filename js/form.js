6737b35c0844c966dc9055ff4ee539463647b973
ceemusi <christophermusitong@gmail.com>
2025-08-02 15:54:37 +0100

    More updates, fixes and files added

--- js/form.js
+++ js/form.js
@@ -1,12 +1,24 @@
 // This was coded by CM x24103446
 
-// Created a function called validate form, 'contactForm' is the form name from the HTML and the ID & name is 'fname' for the properties. It then uses an IF statement for if the user tries to submit nothing, an alert will pop up and tell them that they need to enter the rquired information.
+// Created a function called validate form, 'contactForm' is the form name from the HTM
 function validateForm() {
-  let userInput = document.forms["contactForm"]["fname"].value;
-  if (userInput == "") {
-    alert("Field can not be blank. Please enter the required information.");
-    return false;
-  } else {
-    alert("Thank you! We will be in touch soon.");
+  // Going to put the fieldnames in an array to use later
+  var fieldNames = ['fname', 'lname', 'emailAddy', 'userMessage'];
+  var form = document.forms['contactForm'];
+
+  // Going through each field name, .trim() removes any white spaces between text
+  for (var i = 0; i < fieldNames.length; i++) {
+    var name  = fieldNames[i];
+    var value = form[name].value.trim();
+
+    // If any value is empty, show error and stop
+    if (value === '') {
+      alert('All fields are required. Please enter the required information');
+      return false;
+    } 
   }
-} 
+
+  // Else it will return this when submitted
+  alert('Thank you! We will be in touch soon.');
+  return true;
+}
